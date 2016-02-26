987dddc2b5c105731578232ab17a1ca7490f621b
Patrick Porto <patrick.s.porto@gmail.com>
2016-02-26 15:25:53 -0300

    Configuração do gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,9 @@
+import gulp from 'gulp';
+import nunjucks from 'gulp-nunjucks';
+import config from './config.json';
+
+gulp.task('default', () => {
+    gulp.src('templates/[a-zA-Z0-9]*')
+    .pipe(nunjucks.compile(config))
+    .pipe(gulp.dest('build/hotspot'));
+})
